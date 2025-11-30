e09f02a329752973556e56f28da3bc604c277772
sitebluehost-art <sitebluehost@gmail.com>
2025-11-30 04:49:46 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Load files inside the "public" folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(Website running at http://localhost:${PORT});
+});
